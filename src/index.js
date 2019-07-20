dd362842d823c2f95eabe35eb2ca5432f7bb017e
klauza <klauza.uk@gmail.com>
2019-07-20 14:03:44 +0100

    fixed loading problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import "./style.scss";
 import { app } from "./js/app.js"; 
 
-window.addEventListener('load', function(){
+
+
+function loadImageAsync(url){
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.addEventListener('load', event => resolve(img));
+    img.addEventListener('erorr', reason => reject(new Error('error')));
+
+    img.src = url;
+  })
+}
+
+const bgCss = 
+ `background-image: linear-gradient(rgba(0, 0, 0, 0.25), 
+  rgba(0, 0, 0, 0.25)),
+  url("https://user-images.githubusercontent.com/43997053/61578935-fffd2380-aaf5-11e9-9839-a0fff0e2d490.jpg");`;
+
+
+ window.addEventListener('DOMContentLoaded', () => {
+  loadImageAsync("https://user-images.githubusercontent.com/43997053/61578935-fffd2380-aaf5-11e9-9839-a0fff0e2d490.jpg")
+  .then(img => document.querySelector('.customHeader').style.cssText = bgCss)
+  .then(
+    app(),
+    document.querySelector('.loader').classList.add('hidden')
+    )
+  .catch(reason => console.log(reason));
   
-  this.setTimeout(()=>{
-    const loader = document.querySelector('.loader');
-    app();
-    loader.classList.add('hidden');
-  }, 250)
- 
+  
 });
